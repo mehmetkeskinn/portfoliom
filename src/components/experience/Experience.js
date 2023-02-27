ed1799feaecdd1182e16b3c26a864fa81142c210
Mehmet Keskin <mehmetkeskin2696@gmail.com>
2023-02-27 16:26:39 -0500

    Initial commit

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -10,7 +10,7 @@ const Experience = () => {
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
-            <h3> UI/UX Design</h3>
+            <h3> Front End Developer</h3>
           </div>
           <ul className="service__list">
             <li>
--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../../assets/keskinmehmetcv.pdf";
+import CV from "../../assets/keskinmehmetcv2702.pdf";
 
 const CTA = () => {
   return (
